0370821286f82b3f0af3042df9d24af8cacfca98
rgomide <renato.s.gomide@gmail.com>
2022-01-08 19:09:13 -0300

    dockerize project

--- config/dev.exs
+++ config/dev.exs
@@ -2,12 +2,9 @@ import Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "sysdba",
-  database: "discuss_dev",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+url: System.get_env("DATABASE_URL"),
+show_sensitive_data_on_connection_error: true,
+pool_size: 10
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
@@ -18,7 +15,7 @@ config :discuss, Discuss.Repo,
 config :discuss, DiscussWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
